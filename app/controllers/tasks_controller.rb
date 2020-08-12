cecba24c5400bafd4f19088425d2eb2599ff7f92
Houkai Qian <871789801@qq.com>
2020-08-12 12:21:48 -0400

    left

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def view
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "tasks", to: "tasks#index"
+
+  get "tasks/create", to: "tasks#new"
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id", to: "tasks#view", as: :task
 end
